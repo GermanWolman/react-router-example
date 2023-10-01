74e3f0d8c3f6786d519add780b4eaa9110b1a6db
unknown <gwolman2424@gmail.com>
2023-10-01 14:39:43 -0300

    cuarto commit

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router";
 import { useFetch } from "../hooks/useFetch";
+//import { useFetch } from "../hooks/UseFetch";
 
 const BlogDetails = () => {
   // useParams es un Hook para capturar los parametros pasados por url
